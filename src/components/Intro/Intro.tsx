f1a143906ee2c0062c665fe1437798c67efff641
giovani <giovaniocan@gmail.com>
2023-07-12 08:53:30 -0300

    intro componente is full responsive

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -3,7 +3,7 @@ import { SimpleCarousel } from './Carousel'
 export function Intro() {
   return (
     <div className="mt-36 px-10   w-full flex flex-col-reverse items-center  md:flex-row justify-around gap-8 font-semibold  ">
-      <div className="text-2xl flex text-center  md:w-1/2 md:text-6xl">
+      <div className="flex text-center  md:w-1/2 text-2xl lg:text-6xl">
         <h2>
           Descubra o sabor autêntico e irresistível do{' '}
           <span className="text-yellow-400">pão de queijo</span> em cada
@@ -11,7 +11,7 @@ export function Intro() {
         </h2>
       </div>
 
-      <div className=" md:w-1/3">
+      <div className="mx-10 md:w-1/3 md:m-0">
         <SimpleCarousel />
       </div>
     </div>
