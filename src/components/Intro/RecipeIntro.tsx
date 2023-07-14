4f8c3ee6f516672df6a5860df33290a73ca04987
giovani <giovaniocan@gmail.com>
2023-07-14 14:06:01 -0300

    added recipes content

--- src/components/Intro/RecipeIntro.tsx
+++ src/components/Intro/RecipeIntro.tsx
@@ -1,7 +1,7 @@
 export function RecipeIntro() {
   return (
     <div className="w-full mt-36    ">
-      <div className="flex text-center md:m-10 font-medium  text-2xl md:text-3xl  xl:text-6xl">
+      <div className="flex text-center md:m-10 font-medium  text-xl md:text-2xl  xl:text-5xl">
         <h2>
           Preparar <span className="text-yellow-400">chipa</span> e{' '}
           <span className="text-yellow-400">pão de queijo</span> fresquinho e
---
+++ src/utils/Recipes.ts
@@ -0,0 +1,32 @@
+export const Recipes = [
+  {
+    id: 1,
+    title: 'Pão de queijo tradicional',
+    timeOfPreparation: '20 minutos',
+    distance: '1 cm',
+    temperature: '220°C a 250° C',
+    steps: [
+      'Pre-aqueça o forno a temperatura média de 220°C por 10 minutos.',
+      'Unte a forma com manteiga ou óleo.',
+      'Com uma colher de sorvete ou de sopa, faça as bolinhas e coloque-as na forma com um espaço regular de 1 cm.',
+      ' Asse por aproximadamente 20 minutos em temperatura de 220°C a 250° C.',
+      'Retirar do forno quando o pão de queijo estiver dourado.',
+    ],
+  },
+  {
+    id: 2,
+    title: 'Produtos congelados',
+    timeOfPreparation: '25 minutos',
+    distance: '5 cm',
+    temperature: '180°C a 200° C',
+    steps: [
+      'Pre-aqueça o forno a temperatura média de 220°C por 10 minutos.',
+      'Não há a necessidade de untar a forma.',
+      'Colocar as chipas ou os pães de queijo congelados na assadeira/forno com espaço de 5 cm entre cada.',
+      'Asse por aproximadamente 25 minutos em temperatura de 220°C a 250° C.',
+      'Retirar do forno quando o pão de queijo ou a chipa estiver dourado.',
+    ],
+  },
+]
+
+export const Notes = ['Não assar em microondas.', 'Não precisa descongelar.']
