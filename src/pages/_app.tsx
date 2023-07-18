bffdd1d5421c1e32a877f4f221fb07f594a8fb5a
giovani <giovaniocan@gmail.com>
2023-07-18 17:14:41 -0300

    changed the font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
+/* eslint-disable camelcase */
+
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Nunito_Sans } from 'next/font/google'
+
+const nunito = Nunito_Sans({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={nunito.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
